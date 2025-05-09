a7854c3871ec74e20f9baa465d56e2d662f66686
Nosylax <alyson.laville@gmail.com>
2025-05-09 16:18:53 +0200

    uptade test

--- cypress/e2e/Project.cy.js
+++ cypress/e2e/Project.cy.js
@@ -1,9 +1,12 @@
 describe("Mes Projets", () => {
   it("Je vérifie que les projets sont bien présente", () => {
     cy.visit("/");
-    cy.get("#portfolio > .container > .row > .col-lg-4").should(
-      "have.length",
-      3
-    );
+    cy.readFile("data/skills.json").then((data) => {
+      const projectsLength = data.length;
+      cy.get("#skills > .container > .row > .col-lg-4").should(
+        "have.length",
+        projectsLength
+      );
+    });
   });
 });
--- cypress/e2e/Skills.cy.js
+++ cypress/e2e/Skills.cy.js
@@ -1,6 +1,12 @@
 describe("Mes compétences", () => {
   it("Je vérifie que les compétences sont bien présente", () => {
     cy.visit("/");
-    cy.get("#skills > .container > .row > .col-lg-4").should("have.length", 6);
+    cy.readFile("data/skills.json").then((data) => {
+      const skillsLength = data.length;
+      cy.get("#skills > .container > .row > .col-lg-4").should(
+        "have.length",
+        skillsLength
+      );
+    });
   });
 });
